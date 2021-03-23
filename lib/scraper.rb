1add5537bc171b69e69094deab9c685eb096c694
gajen <meetgajen@yahoo.com>
2021-03-23 16:48:06 -0400

    commit tree 37eb65ee356c6e5dd54a1eccee8df3175890a9e6

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,14 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+      
+
+
+
   end
 
   def self.scrape_profile_page(profile_url)
